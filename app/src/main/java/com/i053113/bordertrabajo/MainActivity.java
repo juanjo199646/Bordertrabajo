d1003c818712b226a1be89c9fb4fe4f92b812fa4
juanjo199646 <juanjo199646@hotmail.com>
2017-09-03 18:13:13 -0500

    otro

--- app/src/main/java/com/i053113/bordertrabajo/MainActivity.java
+++ app/src/main/java/com/i053113/bordertrabajo/MainActivity.java
@@ -2,6 +2,9 @@ package com.i053113.bordertrabajo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
